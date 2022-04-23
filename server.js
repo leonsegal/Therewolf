2e1dae862f5366a5214d864443e017d2d9ed2a78
leon segal <leonsegal1@gmail.com>
2022-04-23 15:57:08 +0100

    add game start and day/night

--- server.js
+++ server.js
@@ -7,52 +7,31 @@ let io = new Server(server);
 let users = [];
 let messages = [];
 let roles = ["werewolf", "warlock", "seer", "hunter", "villager"];
+let isGameStarted = false;
+let phase = "day";
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 io.on("connection", (socket) => {
   // receive on socket, emit on io
   socket.on("user connected", (name) => {
+    // todo: deal with game already started
+
     users.push({ name, id: socket.id });
 
     io.emit("user connected", { users, messages });
 
     // fixme: make higher
-    if (users.length > 2) {
+    if (users.length > 1) {
       startGame();
     }
   });
-
-  function startGame() {
-    let shuffledRoles = shuffle(roles);
-    users.forEach((user, i) => {
-      user.role = shuffledRoles[i];
-      io.to(user.id).emit("start game", user.role);
-    });
-    console.log(users);
-  }
-
-  function shuffle(array) {
-    let currentIndex = array.length;
-    let randomIndex;
-
-    // While there remain elements to shuffle.
-    while (currentIndex !== 0) {
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
-
-    return array;
-  }
 
   socket.on("disconnect", () => {
     users = users.filter((user) => user.id !== socket.id);
@@ -65,6 +44,32 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+function startGame() {
+  isGameStarted = true;
+  phase = "night";
+  let shuffledRoles = shuffle(roles);
+  users.forEach((user, i) => {
+    user.role = shuffledRoles[i];
+    io.to(user.id).emit("start game", user.role);
+  });
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
