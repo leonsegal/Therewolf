f03d61b09fc95898d497651cf28e9c7dfd2eb771
leon segal <leonsegal1@gmail.com>
2022-04-24 21:08:30 +0100

    rename game is ongoing
    - also add role modal logic

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,12 +3,17 @@ let socket = io(); // declared at top of scope
 let isGameOngoing =
   sessionStorage.getItem("isGameOngoing") &&
   sessionStorage.getItem("isGameOngoing") === "true";
+
+let playerSection = document.querySelector("#players ul");
+let messagePanel = document.querySelector("#messages");
+
 let introModal = document.getElementById("intro-modal");
 let introModalClose = document.getElementById("intro-modal-close");
 introModalClose.onclick = () => (introModal.style.display = "none");
 
-let playerSection = document.querySelector("#players ul");
-let messagePanel = document.querySelector("#messages");
+let roleModal = document.getElementById("role-modal");
+let roleModalClose = document.getElementById("role-modal-close");
+roleModalClose.onclick = () => (roleModal.style.display = "none");
 
 let outroModal = document.getElementById("outro-modal");
 let outroModalClose = document.getElementById("outro-modal-close");
@@ -17,6 +22,10 @@ outroModalClose.onclick = () => (outroModal.style.display = "none");
 window.onclick = (event) => {
   if (event.target === introModal) {
     introModal.style.display = "none";
+  }
+
+  if (event.target === roleModal) {
+    roleModal.style.display = "none";
   }
 
   if (event.target === outroModal) {
@@ -32,8 +41,7 @@ socket.on("user connected", ({ users, messages }) => {
   if (!isGameOngoing) {
     introModal.style.display = "block";
   }
-  isGameOngoing = true;
-  sessionStorage.setItem("isGameOngoing", "true");
+
   playerSection.innerHTML = buildUsers(users);
   messagePanel.innerHTML = buildMessages(messages);
 
@@ -53,6 +61,8 @@ socket.on("chat message", (messages) => {
 socket.on("start game", (role) => {
   userRole = role;
   alert(`You are ${role}`);
+  isGameOngoing = true;
+  sessionStorage.setItem("isGameOngoing", "true");
 });
 
 function getName() {
